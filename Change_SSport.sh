fa0072e9057f7c60a9c6d8566873550d91fdde6c
lxyok <lxy30383503@gmail.com>
2019-11-02 17:49:58 +0800

    Update Change_SSport.sh

--- Change_SSport.sh
+++ Change_SSport.sh
@@ -5,17 +5,17 @@ export PATH
 # @Description: 快速修改SS配置文件的端口和密码
 # @Autor: lxyok
 # @Date: 2019-09-25 14:36:45
- # @LastEditors: lxyok
- # @LastEditTime: 2019-10-06 21:21:07
+ # @LastEditors: Please set LastEditors
+ # @LastEditTime: 2019-11-02 17:49:51
 ###
 
 change_port() {
     while true
     do
-        dport=$(shuf -i 9000-49999 -n 1)
+        port=$(shuf -i 9000-49999 -n 1)
         echo -e "Please enter a port for ${software[${selected}-1]} [1-65535]"
-        read -p "(Default port: ${dport}):" shadowsocksport
-        [ -z "${shadowsocksport}" ] && shadowsocksport=${dport}
+        read -p "(Default port: ${port}):" shadowsocksport
+        [ -z "${shadowsocksport}" ] && shadowsocksport=${port}
         expr ${shadowsocksport} + 1 &>/dev/null
         if [ $? -eq 0 ]; then
             if [ ${shadowsocksport} -ge 1 ] && [ ${shadowsocksport} -le 65535 ] && [ ${shadowsocksport:0:1} != 0 ]; then
